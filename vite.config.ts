1d00e7ad15f2722a090267c880f2b5f107b487de
Sammwy <sammwy.dev@gmail.com>
2025-11-30 11:51:03 -0300

    feat: 🐷 remove gh actions, replaced by gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/biovis/' : '/',
+  base: process.env.USE_PATH_PREFIX === 'true' ? '/biovis/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
