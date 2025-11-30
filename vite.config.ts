82d496c5f51a8bd7a4c816a3126e795241cae73b
Sammwy <sammwy.dev@gmail.com>
2025-11-30 11:43:52 -0300

    feat: 😼 added github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? '/biovis/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
